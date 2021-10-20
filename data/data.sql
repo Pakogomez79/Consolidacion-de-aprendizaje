181b172e9cd46828fb51e61dcb808e740f01e321
maumatus <86168992+maumatus@users.noreply.github.com>
2021-10-20 15:02:04 -0300

    avance 1

--- data/data.sql
+++ data/data.sql
@@ -7,3 +7,9 @@ CREATE TABLE deportistas(
 	peso DECIMAL NOT NULL
 	
 );
+
+INSERT INTO deportistas(rut,nombre,fecha_nac,estatura,peso) VALUES('12.456.123-5','Juan','1990-05-10','1.80',73);
+INSERT INTO deportistas(rut,nombre,fecha_nac,estatura,peso) VALUES('14.456.123-5','Pedro','1990-05-10','1.82',65);
+INSERT INTO deportistas(rut,nombre,fecha_nac,estatura,peso) VALUES('12.556.123-5','Osman','1993-07-10','1.60',61);
+INSERT INTO deportistas(rut,nombre,fecha_nac,estatura,peso) VALUES('14.656.123-5','Luis','1995-06-12','1.85',63);
+INSERT INTO deportistas(rut,nombre,fecha_nac,estatura,peso) VALUES('15.656.123-5','Pablo','1991-09-12','1.83',60);
--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const http = require("http");
-const fs = require("fs")
-    // Esto es
+const fs = require("fs");
+const {Pool}= require("pg");
+//const Cursor = require('pg-cursor');
+    
+//Configuracion de conexión
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'deportistas',
+    password:'postgres',
+    port:5432,
+    max:20,
+    min:2,
+    idleTimeoutMillis:5000,
+    connectionTimeoutMillis:2000
+});
 
+//Logica API CRUD
 http.createServer((req, res) => {
+    
+    //Respuesta al requerir ruta base
     if (req.url == '/' && req.method == 'GET') {
         res.setHeader('content-type', 'text/html')
         res.end(fs.readFileSync('./public/index.html', 'utf8'))
-    }
+    };
+    //Endpoint para listar deportistas que estan dentro de BD. Mismo boton debiera registrar y listar.
+    if (req.url == '/registrar' && req.method == 'POST') {
+        pool.connect(async(err,client,release)=>{
+            if(!err){
+                let sqlQuery = {
+                    text:"SELECT * FROM deportistas",
+                    name: 'competidores',
+                    rowMode:'array'
+                }
+                let res = await client.query(sqlQuery)
+                release()
+                console.log(res.rows)
+                
+            }if(err){
+                console.log(err)
+            }
+            pool.end()
+        });  
+    };
+
+
 }).listen(3000, () => console.log("Servidor activo http://localhost:3000"))
