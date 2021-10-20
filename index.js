a826407cb6eba713de257835e590681e383e0572
Pedro Pablo Belmar Van-Schuerbeck <pbelmarv@hotmail.com>
2021-10-20 11:28:05 -0300

    Habilitando vista principal & Bootstrap

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require("http");
+const fs = require("fs")
 
-http.createServer((req,res) => {
-    res.end("Servidor activo")
-}).listen(3000,() => console.log("Servidor activo http://localhost:3000"))
+http.createServer((req, res) => {
+    if (req.url == '/' && req.method == 'GET') {
+        res.setHeader('content-type', 'text/html')
+        res.end(fs.readFileSync('./public/index.html', 'utf8'))
+    }
+}).listen(3000, () => console.log("Servidor activo http://localhost:3000"))
