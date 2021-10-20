830bfb7ab35b8c17101a44be7dfdbd4f5fc0d6b4
Pedro Pablo Belmar Van-Schuerbeck <pbelmarv@hotmail.com>
2021-10-20 12:39:28 -0300

    Recuperar index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const fs = require("fs")
+    // Esto es
+
+http.createServer((req, res) => {
+    if (req.url == '/' && req.method == 'GET') {
+        res.setHeader('content-type', 'text/html')
+        res.end(fs.readFileSync('./public/index.html', 'utf8'))
+    }
+}).listen(3000, () => console.log("Servidor activo http://localhost:3000"))
